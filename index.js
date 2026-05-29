32c86c2b8b4ac4be740e925c4a4ed3b84f5f34b2
Petalum <giftedgarret@gmail.com>
2026-05-29 14:31:28 +0300

    Add index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+const sequelize = require('./db');
+const models = require('./models/models.js');
+const cors = require('cors');
+const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware.js');
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
+app.use(errorHandler);
+
+const start = async () => {
+    try {
+        // Подключение к базе данных.
+        await sequelize.authenticate();
+        // Сверим состояние базы данных со схемой.
+        await sequelize.sync({ alter: true });
+
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
