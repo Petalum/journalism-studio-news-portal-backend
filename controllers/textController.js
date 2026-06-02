d957638154f6345be970563abe16d1dbf6bb1b2c
Petalum <giftedgarret@gmail.com>
2026-06-02 15:07:05 +0300

    Add img creation logic to text controller

--- controllers/textController.js
+++ controllers/textController.js
@@ -1,6 +1,22 @@
-class TextController {
-    async createText(req, res) {
+const uuid = require('uuid');
+const path = require('path');
+const { Text } = require('../models/models');
+const ApiError = require('../error/apiError');
 
+/** Controller for working with texts. */
+class TextController {
+    async createText(req, res, next) {
+        try {
+            const { body, title } = req.body;
+            const { img } = req.files;
+            const fileName = uuid.v4() + '.jpg';
+            img.mv(path.resolve(__dirname, '..', 'static', fileName));
+
+            const text = await Text.create({ body, title, img: fileName });
+            return res.json(text);
+        } catch (e) {
+            next(ApiError.badRequest(e.message));
+        }
     }
 
     async changeText(req, res) {
@@ -8,15 +24,15 @@ class TextController {
     }
 
     async getAll(req, res) {
-        
+
     }
 
     async getOne(req, res) {
-        
+
     }
 
     async deleteOne(req, res) {
-        
+
     }
 }
 
