2f2938d6a81096a098dc8e8e0bdf93381750db79
Petalum <giftedgarret@gmail.com>
2026-06-04 12:21:27 +0300

    Add text creation logic

--- controllers/textController.js
+++ controllers/textController.js
@@ -1,18 +1,25 @@
 const uuid = require('uuid');
 const path = require('path');
-const { Text } = require('../models/models');
+const { Text, User } = require('../models/models');
 const ApiError = require('../error/apiError');
+const { Op } = require('sequelize');
 
 /** Controller for working with texts. */
 class TextController {
     async createText(req, res, next) {
         try {
-            const { body, title } = req.body;
+            const { body, title, dt_publish, dt_edit, authors_id } = req.body;
             const { img } = req.files;
             const fileName = uuid.v4() + '.jpg';
             img.mv(path.resolve(__dirname, '..', 'static', fileName));
 
-            const text = await Text.create({ body, title, img: fileName });
+            const text = await Text.create({ body, title, dt_publish, dt_edit, img: fileName });
+
+            const authors = await User.findAll({ where: { id: { [Op.or]: JSON.parse(authors_id) } } });
+            if (authors && text) {
+                authors.forEach((e) => { e.addText(text.id); });
+            }
+
             return res.json(text);
         } catch (e) {
             next(ApiError.badRequest(e.message));
