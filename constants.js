545cc07ad3aa480ebafedd2afa3044762111447d
Petalum <giftedgarret@gmail.com>
2026-07-11 19:29:30 +0300

    Create text statuses

---
+++ constants.js
@@ -0,0 +1,11 @@
+/** Current text statuses. */
+const TextStatuses = {
+    draft: 'draft',
+    check: 'check',
+    correct: 'correct',
+    published: 'published',
+};
+
+module.exports = {
+    TextStatuses,
+}
