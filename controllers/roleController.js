498c749d52a17bd1d5fa196501f88fc713eef56d
Petalum <giftedgarret@gmail.com>
2026-07-13 20:07:05 +0300

    Create roles error handling and rename methods

--- controllers/roleController.js
+++ controllers/roleController.js
@@ -1,32 +1,40 @@
-const {Role} = require('../models/models');
+const { Role } = require('../models/models');
 const ApiError = require('../error/apiError');
 
 /** Controller for working with user roles. */
 class RoleController {
-    async createOne(req, res) {
-        const {name} = req.body;
-        const role = await Role.create({name});
-        return res.json(role);
+  async createRole(req, res) {
+    try {
+      const { name } = req.body;
+      const role = await Role.create({ name });
+      return res.json(role);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
     }
+  }
 
-     async changeOne(req, res, next) {
-        const {id} = req.params;
-        if (Number.isInteger(+id)) {
-          const role = await Role.findByPk(id);
-          if (role) {
-            const updatedRole = await role.update({name: req.body.name});
-            return res.json(updatedRole);
-          } else {
-            return next(ApiError.badRequest('Роль отсутствует'));
-          }
+  async changeRole(req, res, next) {
+    try {
+      const { id } = req.params;
+      if (Number.isInteger(+id)) {
+        const role = await Role.findByPk(id);
+        if (role) {
+          const updatedRole = await role.update({ name: req.body.name });
+          return res.json(updatedRole);
         } else {
-             return next(ApiError.badRequest('Идентификатор роли не указан'));
+          throw new Error('Роль отсутствует');
         }
+      } else {
+        throw new Error('Идентификатор роли не указан');
+      }
+    } catch (e) {
+      return next(ApiError.badRequest(e.message));
     }
+  }
 
-    async getAll(req, res) {
-        res.json('123');
-    }
+  async getAll(req, res) {
+    res.json('123');
+  }
 }
 
 module.exports = new RoleController();
