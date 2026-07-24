6ef0818af3099ff820034a72035569a6b9c5d981
Petalum <giftedgarret@gmail.com>
2026-07-24 12:20:45 +0300

    Create user service

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,15 @@
 const ApiError = require('../error/apiError');
+const userService = require('../service/userService');
 const { User } = require('../models/models');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const uuid = require('uuid');
-const mailService = require('../service/mailService');
-const tokenService = require('../service/tokenService');
-const UserDto = require('../dtos/userDto');
 
 /** Controller for working with users. */
 class UserController {
     async registration(req, res, next) {
         try {
-            const { name, surname, patronymic, group, email, password, roleId } = req.body;
-            if (!email || !password) {
-                throw new Error('Неверно указан email или пароль');
-            }
-            const foundUser = await User.findOne({ where: { email } });
-            if (foundUser) {
-                throw new Error('Пользователь с таким email уже существует');
-            }
-            const hashPas = await bcrypt.hash(password, 5);
-            const activationLink = uuid.v4();
-            const user = await User.create({ name, surname, patronymic, group, email, roleId, password: hashPas, activationLink });
-            await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
-            const dto = new UserDto(user);
-            const tokens = tokenService.generateTokens({...dto});
-            await tokenService.saveToken(dto.id, tokens.refreshToken);
-            res.cookie('refreshToken', tokens.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
-            return res.json({ ...tokens, user: dto });
+            const userData = await userService.registration(req.body);
+            res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
+            return res.json(userData);
         } catch (e) {
             return next(ApiError.badRequest(e.message));
         }
