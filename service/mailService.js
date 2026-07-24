49a890ea8311ebd07a006ae15ba40428a0a4a2ac
Petalum <giftedgarret@gmail.com>
2026-07-24 11:31:36 +0300

    Add email sending logic to mail service

--- service/mailService.js
+++ service/mailService.js
@@ -1,6 +1,25 @@
+const nodemailer = require('nodemailer');
+
 /** Email sending service. */
 class MailService {
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: process.env.SMTP_IS_SECURE,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD,
+            }
+        })
+    }
     async sendActivationMail(to, link) {
+        await this.transporter.sendMail({
+            from: process.env.SMTP_USER,
+            to,
+            subject: `Активация аккаунта на ${process.env.API_URL}`,
+            text: `Добрый день! Сcылка для активации: ${link}`,
+        })
 
     }
 }
