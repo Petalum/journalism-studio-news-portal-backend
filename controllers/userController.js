904f51a4f13731bf23b87ba7af4f4b1204cf030e
Petalum <giftedgarret@gmail.com>
2026-08-03 20:03:52 +0300

    Add user fields validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,16 @@ const ApiError = require('../error/apiError');
 const userService = require('../service/userService');
 const { User } = require('../models/models');
 const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 
 /** Controller for working with users. */
 class UserController {
     async registration(req, res, next) {
         try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return next(ApiError.badRequest('Ошибка валидации', errors.array()));
+            }
             const userData = await userService.registration(req.body);
             res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
             return res.json(userData);
