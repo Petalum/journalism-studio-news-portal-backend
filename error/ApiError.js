682610a66863727462eb97d6fe3da263741812a3
Petalum <giftedgarret@gmail.com>
2026-08-03 19:48:04 +0300

    Extend api errors list

--- error/ApiError.js
+++ error/ApiError.js
@@ -2,19 +2,23 @@
  * A class for handling errors returned by the server.
  */
 class ApiError extends Error {
-    constructor(status, message, errors=[]) {
+    constructor(status, message, errors = []) {
         super(message);
         this.status = status;
         this.message = message;
         this.errors = errors;
     }
 
-    static badRequest(message, errors=[]) {
-        return new ApiError(404, message, errors);
+    static badRequest(message, errors = []) {
+        return new ApiError(400, message, errors);
     }
 
-    static internal(message, errors=[]) {
+    static internal(message, errors = []) {
         return new ApiError(500, message, errors);
+    }
+
+    static notFound(message) {
+        return new ApiError(404, message);
     }
 
     static forbidden(message) {
