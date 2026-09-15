b9beb4b4df1a371f35782ed496f669fbda252f16
Petalum <giftedgarret@gmail.com>
2026-09-15 11:07:37 +0300

    Add token refresh methods to services

--- service/tokenService.js
+++ service/tokenService.js
@@ -38,6 +38,29 @@ class TokenService {
         const tokenData = await Token.destroy({ where: { refreshToken } });
         return 'Токен удалён';
     }
+
+    async findToken(refreshToken) {
+        const tokenData = await Token.findOne({ where: { refreshToken } });
+        return tokenData;
+    }
+
+    validateAccessToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+            return userData;
+        } catch (e) {
+            return null;
+        }
+    }
+
+    validateRefreshToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+            return userData;
+        } catch (e) {
+            return null;
+        }
+    }
 }
 
 module.exports = new TokenService();
