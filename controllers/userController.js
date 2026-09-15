ae905b7078a3c4613805bb6cd66952aac8cf73c2
Petalum <giftedgarret@gmail.com>
2026-09-15 11:09:34 +0300

    Add token refresh methods to controller and router

--- controllers/userController.js
+++ controllers/userController.js
@@ -52,6 +52,18 @@ class UserController {
         }
     }
 
+    async refresh(req, res, next) {
+        try {
+            const { refreshToken } = req.cookies;
+            const userData = await userService.refresh(email, password);
+            res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
+            return res.json(userData);
+
+        } catch (e) {
+            next(e);
+        }
+    }
+
     async check(req, res, next) {
         const { id } = req.query;
         if (!id) {
